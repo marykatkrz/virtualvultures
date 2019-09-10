c4d98f813f8802f146fc75cf0f8f1fbda3022bdb
Mary Krzesicki <krzesicki.mary@gmail.com>
2019-09-09 21:09:51 -0700

    ready

--- vv.js
+++ vv.js
@@ -1,14 +1,18 @@
+var introText=document.querySelectorAll('.intro-text');
+
 function scrollAppear(){
-    var introText=document.querySelectorAll('.intro-text');
-    
-    for(var i=0; i<introText.length; i++){
+    for(var i=1; i<introText.length; i++){
         var introPosition=introText[i].getBoundingClientRect().top;
-        var screenPosition=window.innerHeight/2;
+        var screenPosition=window.innerHeight;
         if(introPosition<screenPosition){
             introText[i].classList.add('intro-appear');
         }
+        
     }
 }
 window.addEventListener('scroll', scrollAppear);
-
-
+function loadAppear(){
+    var load=introText[0].classList.add('intro-appear')
+    return load
+}
+window.addEventListener('load', loadAppear)
